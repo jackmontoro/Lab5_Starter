13c7efea877e1b4b89596813c557d7c65bfe8af8
jackmontoro <jmontoro@ucsd.edu>
2023-05-07 20:40:39 -0700

    started explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,50 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+var voiceSelect;
+var smileFace;
+var text;
+var play;
+
 function init() {
-  // TODO
+  voiceSelect = document.getElementById("voice-select");
+  const spSynth = window.speechSynthesis;
+  let voiceList = spSynth.getVoices();
+
+  text = document.getElementById("test-to-speak");
+  smileFace = document.querySelector("img[alt='Smiling face']");
+
+
+  function initializeVoiceSelector() {
+    for(let i = 0; i < voiceList.length; i++) {
+      const voice = document.createElement("option");
+      voice.textContent = `${voiceList[i].name} (${voiceList[i].lang})`;
+      voice.setAttribute("data-lang", voiceList[i].lang);
+      voice.setAttribute("data-name", voiceList[i].name);
+      voiceList.appendChild(voice);
+    }
+  }
+
+  speechSynthesis.addEventListener("voiceschanged", () => {
+    voiceList = spSynth.getVoices();
+    initializeVoiceSelector();
+  });
+
+  
+  play = document.querySelector("button");
+
+  play.addEventListener("click", (event) => {
+    text = document.getElementById("test-to-speak");
+    smileFace = document.querySelector("img[alt='Smiling face']");
+    if (text.value != "") {
+      let utter = new SpeechSynthesisUtterance(text.value);
+      utter.onstart = function() {
+        smileFace.src="./assets/images/smiling-open.png";
+      };
+      utter.onend = function() {
+        smileFace.src="./assets/images/smiling.png";
+      };
+    }
+  })
+
 }
